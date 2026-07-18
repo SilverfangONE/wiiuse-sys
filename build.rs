76465786d6cfd7f80d2fac98227348cf0c56429f
SilverfangONE <luis.boden@gmail.com>
2026-07-18 13:43:09 +0200

    init

---
+++ build.rs
@@ -0,0 +1,19 @@
+use std::env;
+use std::path::PathBuf;
+
+fn main() {
+    println!("cargo:rustc-link-lib=wiiuse");
+    println!("cargo:rerun-if-changed=wrapper.h");
+
+    let bindings = bindgen::Builder::default()
+        .header("wrapper.h")
+        .parse_callbacks(Box::new(bindgen::CargoCallbacks::new()))
+        .generate()
+        .expect("couldn't generate bindings");
+
+    // output
+    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+    bindings
+        .write_to_file(out_path.join("bindings.rs"))
+        .expect("couldn't write bindings");
+}
